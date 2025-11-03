ebef7827e58f1234ac0dd0cbb88daa919ff4683c
unknown <talal.kalaji.alep@gmail.com>
2025-11-03 22:25:52 +0300

    update product edit function 2

--- akilcompany/settings.py
+++ akilcompany/settings.py
@@ -136,6 +136,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://akilcompany.com", 
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://localhost:3001",
     "http://localhost:5431",     
 ]
